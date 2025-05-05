a3b9982d36cf4d5313f5413b5126e45ee802cd7b
Hussein Ghazala <husseinghazala39@gmail.com>
2025-05-05 03:30:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: In HTML, whitespace text nodes cannot be a child of <tr>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    > This will cause a hydration error.. Error source: See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    > ...
    >     <ClientPageRoot Component={function ClientTracker} searchParams={{}} params={{}}>
    >       <ClientTracker params={Promise} searchParams={Promise}>
    >         <div className="container ...">
    >           <h1>
    >           <_c>
    >           <_c>
    >           <_c>
    >           <_c className="mb-8 shado...">
    >             <div ref={null} className="bg-card te...">
    >               <_c2>
    >               <_c8 className="pt-0">
    >                 <div ref={null} className="p-6 pt-0">
    >                   <_c>
    >                     <div className="relative w...">
    >                       <table ref={null} className="w-full cap...">
    >                         <_c14>
    >                         <_c2>
    >                         <_c4>
    >                           <tbody ref={null} className={"[&_tr:la..."}>
    >                             <_c8 className="hover:bg-m...">
    > >                             <tr
    > >                               ref={null}
    > >                               className="border-b data-[state=selected]:bg-muted hover:bg-muted/30 transition-colors..."
    > >                             >
    >                                 <_c12>
    >                                 <_c12>
    >                                 <_c12>
    >                                 <_c12>
    >                                 <_c12>
    >                                 <_c12>
    >                                 <_c12>
    >                                 <_c12>
    >                                 <_c12>
    > >                               {" "}
    >                                 ...
    >                             ...
    >                         ...
    >           ...
    >     ...
    >
    >
    > src/components/ui/table.tsx (58:3) @ _c8
    >
    >   56 |   React.HTMLAttributes<HTMLTableRowElement>
    >   57 | >(({ className, ...props }, ref) => (
    > > 58 |   <tr
    >      |   ^
    >   59 |     ref={ref}
    >   60 |     className={cn(
    >   61 |       "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
    >
    > Call Stack
    > 25
    >
    > Show 20 ignore-listed frame(s)
    > tr
    > <anonymous> (0:0)
    > _c8
    > src/components/ui/table.tsx (58:3)
    > <unknown>
    > src/app/page.tsx (1454:23)
    > Array.map
    > <anonymous> (0:0)
    > ClientTracker
    > src/app/page.tsx (1449:31)

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -54,7 +54,7 @@ TableFooter.displayName = "TableFooter"
 const TableRow = React.forwardRef<
   HTMLTableRowElement,
   React.HTMLAttributes<HTMLTableRowElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => ( // Explicitly include children
   <tr
     ref={ref}
     className={cn(
@@ -62,7 +62,9 @@ const TableRow = React.forwardRef<
       className
     )}
     {...props}
-  />
+  >
+    {children} {/* Render children explicitly inside the tr */}
+  </tr>
 ))
 TableRow.displayName = "TableRow"
 
